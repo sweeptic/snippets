0e92ad78af0a1302b4ef4e9493748ca3d48617dc
Attila Horvath <sweeptic@gmail.com>
2025-05-06 11:44:20 +0200

    05. Adding Dynamic Paths

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,11 @@ export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <div key={snippet.id}>
+        {snippet.id}, {snippet.title}, {snippet.code}
+      </div>
+    );
   });
 
   return <div>{renderedSnippets}</div>;
---
+++ app/snippets/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default function SnippetShowPage(props: any) {
+  console.log('props', props);
+
+  return <div>Show a snippet</div>;
+}
